11694d1e3feb70eb13164f02e21578894ea61988
Andrij Demudenko <demedp.s@gmail.com>
2023-11-28 12:41:55 +0200

    Add project files.

---
+++ Coursework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coursework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
